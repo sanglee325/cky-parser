2405c3bcfbb8b9417d0c1c19e1f5e06e86d36a74
sanglee <pksll325@gmail.com>
2021-04-03 02:40:19 +0900

    assignment done roughly

--- data_loader.py
+++ data_loader.py
@@ -30,8 +30,12 @@ class grammar:
             for p in self.rule[r]:
                 print(p, '->', r)
 
-    def applyRule(self, key):
-        return self.rule[key]
+    def getRule(self, key):
+        try: 
+            val = self.rule[key]
+            return val
+        except:
+            return None
 
 class sentence:
     def __init__(self):
@@ -40,11 +44,103 @@ class sentence:
     def load(self, path):
         try:
             self.content = np.loadtxt(path, dtype='str', delimiter='\t', encoding='utf-8')
-            print(self.content)
             return self.content
         except:
             print("[ERROR] loading sentence failed")
         
         return None
 
+class production:
+    def __init__(self, g_data, pos1=None, pos2=None, p1=None, p2=None):
+        self.result = []
+        self.pos1 = pos1
+        self.pos2 = pos2
+        self.p1 = p1
+        self.p2 = p2
+        self.node_stack = []
+        self.g_data = g_data
+
+    def get_result(self):
+        if self.check_overlap() is False:
+            self.result = None
+            return
+        
+        if self.pos1 == None:
+            self.result = None
+        else:
+            if self.pos2 == None:
+                tmp = self.g_data.getRule(self.pos1)
+            elif self.pos1 != None and self.pos2 != None:
+                #print(self.pos1, self.pos2)
+                tmp = self.g_data.getRule(self.pos1+" "+self.pos2)
+            
+            if tmp == None:
+                self.result = tmp
+            else:
+                self.result = tmp[0]
+            
+        #print('production: ', self.result)
+        return self.result
+    
+    def convert_word(self):
+        if self.pos2 == None:
+           self.result = self.g_data.getRule(self.pos1)
+        return self.result
+    
+    def check_overlap(self):
+        if self.p1 is not None and len(self.p1.get_stack()) == 0:
+            self.node_stack.append(self.p1)
+        if self.p2 is not None and len(self.p2.get_stack()) == 0:
+            self.node_stack.append(self.p2)
+        
+        for i in self.p1.get_stack():
+            if i in self.p2.get_stack():
+                return False
+        
+        if self.p1 is not None:
+            self.node_stack += self.p1.get_stack()
+        if self.p2 is not None:
+            self.node_stack += self.p2.get_stack()
+
+        return True
+
+
+    def get_value(self):
+        return self.result
+
+    def get_left(self):
+        return self.p1
+    
+    def get_right(self):
+        return self.p2
+
+    def get_pos1(self):
+        return self.pos1
+    
+    def get_stack(self):
+        return self.node_stack
+
+class cell:
+    def __init__(self):
+        self.productions = None
+        self.element = 0
+    
+    def add_prod(self, production):
+        if production != None:
+            if self.productions == None:
+                self.productions = []
+            self.productions.append(production)
+
+    
+    def print_cell(self):
+        if self.productions == None:
+            print("None", end=' ')
+        else:
+            print('[', end='')
+            for p in self.productions:
+                print(p.get_value(), end=' ')
+            print(']', end=' ')
+
+    def get_prod(self):
+        return self.productions
 
--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import numpy as np
 
-from data_loader import grammar, sentence
+from data_loader import *
 
-def printCKY(table):
+def printCKYtable(table):
     for i in table:
-        print(i)
+        for j in i:
+            j.print_cell()
+        print()
+
+def printCKYtree(root, prev=None):
+    if root is None:
+        return
+    print('[', end='')
+    if root.get_left() is None and root.get_right() is None:
+        print(root.get_pos1(), end=': ')
+        print(prev.get_pos1(), end=' ')
+    else:
+        print(root.get_value(), end=' ')
+    printCKYtree(root.get_left(), root)
+    printCKYtree(root.get_right(), root)
+    print(']', end='')
+
+def printSent(root, prev=None):
+    if root is None:
+        return
+    #print('[', end='')
+    if root.get_left() is None and root.get_right() is None:
+        print(root.get_pos1(), end=' ')
+        #print(prev.get_pos1(), end=' ')
+    printSent(root.get_left(), root)
+    printSent(root.get_right(), root)
+    #print(']', end='')
+
 
 if __name__=='__main__':
     # load grammar
@@ -21,15 +48,50 @@ if __name__=='__main__':
         #print(split_sent)
         n = len(split_sent)
 
-        cky_table = [[[] for i in range(n+1)] for j in range(n)]
+        cky_table = [[cell() for i in range(n+1)] for j in range(n)]
 
+        # fill in diagonal table
         for i in range(n):
-            cky_table[i][i+1] = G.applyRule(split_sent[i])
-
+            p = production(G, pos1=split_sent[i], pos2=None)
+            p.convert_word()            
+            for pos in p.get_value():
+                convert = production(G, pos1=pos, p1=p)
+                convert.get_result()
+                #convert = production(G, pos1=pos, pos2=None).get_result()
+                if convert.get_value() != None:
+                    cky_table[i][i+1].add_prod(convert)
         
-        printCKY(cky_table)
-
-
-
+        # run cky parsing algorithm
+        for i in range(1, n):
+            for j in range(i, 0, -1):
+                c = i+1
+                r = j-1
+                for k in range(1, c-r):
+                    for l in range(1, c-r):
+                        t1 = cky_table[r][c-k]
+                        t2 = cky_table[r+l][c]
+                        
+                        if t1.productions != None and t2.productions != None:
+                            for a in t1.productions:
+                                for b in t2.productions:
+                                    #print(a.get_value(), b.get_value())
+                                    p = production(pos1=a.get_value(), pos2=b.get_value(), g_data=G, 
+                                                    p1=a, p2=b)
+                                    p.get_result()
+                                    #p = production(pos1=a, pos2=b, g_data=G).get_result()
+                                    if p.get_value() != None:
+                                        cky_table[r][c].add_prod(p)
+                            break
+        
+        #printCKYtable(cky_table)
+        
+        for r in cky_table:
+            for c in r:
+                if c.get_prod() != None:
+                    for p in c.get_prod():
+                        if p.get_value() == 'S':
+                            #printSent(p)
+                            printCKYtree(p)
+                            print()
 
 
